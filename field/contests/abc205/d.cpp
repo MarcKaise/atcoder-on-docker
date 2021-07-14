b184b0232dbc777ae4b53f75f3910c3ef21dd59c
mkaise <masanori.kaise@woven-planet.global>
2021-07-14 00:13:14 +0000

    fixed logic in abc205

--- field/contests/abc205/d.cpp
+++ field/contests/abc205/d.cpp
@@ -2,30 +2,32 @@
 using namespace std;
 using ll = long long;
 
+const ll K_MAX = 10e5;
+
 int main() {
   ll N, Q;
   cin >> N >> Q;
 
-  vector<ll> A(N);
-  for (int i=0; i<N; ++i) {
+  vector<ll> A(N+1);
+  vector<ll> DiffList(N+1);
+  A[0] = 0;
+  DiffList[0] = 0;
+  for (int i = 1; i < N+1; ++i) {
     cin >> A[i];
+    DiffList[i] = A[i] - i;
   }
-
+  
   ll K;
-  for (int q=0; q<Q; ++q) {
+  while (Q--) {
     cin >> K;
-    ans = 0;
-    if ((A[N-1] - N) <= K) {
-      ans = A[N-1] + (K - (A[N-1] - N));
+    if (DiffList[N] < K) {
+      // KがAの範囲外の場合
+      cout << K + N << endl;
     } else {
-      ll st = 0;
-      ll ed = N;
-      while ()
+      // KがAの範囲内の場合
+      auto skipped = lower_bound(DiffList.begin(), DiffList.end(), K) - DiffList.begin();
+      cout << A[skipped - 1] + (K - DiffList[skipped - 1]) << endl;
     }
-
-
-
   }
-
-
+  return 0;
 }
